3a799940c75843cae4c7d741ba3f02909acde1b8
j- <j@skeoh.com>
2022-12-14 18:04:54 +1100

    Remove default content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppType } from 'next/app';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
