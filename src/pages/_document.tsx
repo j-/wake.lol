81c494a2018ec27ac172b59427c2c81b1d297ba1
j- <j@skeoh.com>
2022-12-17 17:21:48 +1100

    Add lang tag to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default class MyDocument extends Document {
   render() {
     const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps;
     return (
-      <Html className={pageProps.documentClassName}>
+      <Html lang="en" className={pageProps.documentClassName}>
         <Head />
         <body className={pageProps.bodyClassName}>
           <Main />
