4c31f774564764fde1b087c3b8c528ca34f07358
j- <j@skeoh.com>
2023-01-07 23:52:07 +1100

    Add Google Analytics types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,7 @@ const App: AppType = ({ Component, pageProps }) => {
         <meta name="theme-color" content="#000000" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
       </Head>
+      <Component {...pageProps} />
       <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-PLZRPEP9E7" />
       <Script
         id="google-analytics"
@@ -37,7 +38,6 @@ const App: AppType = ({ Component, pageProps }) => {
           `,
         }}
       />
-      <Component {...pageProps} />
     </>
   );
 };
