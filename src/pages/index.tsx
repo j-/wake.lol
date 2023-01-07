fdc6277f074627bc8bf5b0f04a1d53d4fadb27cc
j- <j@skeoh.com>
2023-01-07 11:45:09 +1100

    Remove icon React elements in favor of SVG imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,11 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
 import { useCallback, useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css';
 import svgActive from '../icons/active.inline.svg';
 import svgInactive from '../icons/inactive.inline.svg';
 import { getWakeLockSentinel } from '../wake-lock-sentinel';
-
-const eyeClosed = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    className={styles.indicatorIcon}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M13.875 18.825A10.05 10.05 0 0 1 12 19c-4.478 0-8.268-2.943-9.543-7A9.97 9.97 0 0 1 4.02 8.971m5.858.908a3 3 0 1 1 4.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88 6.59 6.59m7.532 7.532 3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0 1 12 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 0 1-4.132 5.411m0 0L21 21"
-    />
-  </svg>
-);
-
-const eyeOpen = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    className={styles.indicatorIcon}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-    />
-  </svg>
-);
 
 const useIsInitialized = () => {
   const [isInitialized, setIsInitialized] = useState(false);
@@ -143,7 +110,23 @@ const HomePage: NextPage = () => {
         title={isActive ? 'Wake lock is enabled, click to disable' : 'Wake lock is disabled, click to enable'}
         disabled={!isInitialized}
       >
-        {isActive ? eyeOpen : eyeClosed}
+        {
+          isActive ?
+            <Image
+              className={styles.indicatorIcon}
+              src={svgActive}
+              alt="Open eye icon"
+              width={64}
+              height={64}
+            /> :
+            <Image
+              className={styles.indicatorIcon}
+              src={svgInactive}
+              alt="Closed eye icon"
+              width={64}
+              height={64}
+            />
+        }
       </button>
     </>
   );
