4a03dfa42e0c830a541132cc6f376d80c6c193ba
j- <j@skeoh.com>
2023-01-08 00:53:49 +1100

    Add "open in new window" button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import classNames from 'classnames';
 import styles from '../styles/Home.module.css';
 import svgActive from '../icons/active.inline.svg';
 import svgInactive from '../icons/inactive.inline.svg';
+import svgNewWindow from '../icons/window.inline.svg';
 import { getWakeLockSentinel } from '../wake-lock-sentinel';
+
+const BASE_URL = 'https://wake.lol/';
+const PARAM_STANDALONE = 'standalone';
+const PARAM_NEW_WINDOW = 'newwindow';
+const FEATURES = 'width=300,height=200';
 
 const useIsInitialized = () => {
   const [isInitialized, setIsInitialized] = useState(false);
@@ -17,11 +24,26 @@ const useIsInitialized = () => {
   return isInitialized;
 };
 
+const usePageUrl = () => {
+  const isInitialized = useIsInitialized();
+  return isInitialized ? new URL(window.location.href) : new URL(BASE_URL);
+};
+
 const HomePage: NextPage = () => {
   const [isEnabled, setIsEnabled] = useState(false);
   const [sentinel, setSentinel] = useState<WakeLockSentinel>();
   const isInitialized = useIsInitialized();
   const isActive = isEnabled && sentinel != null;
+
+  const pageUrl = usePageUrl();
+
+  const isStandalone = useMemo(() => {
+    return pageUrl.searchParams.has(PARAM_STANDALONE);
+  }, [pageUrl]);
+
+  const isNewWindow = useMemo(() => {
+    return pageUrl.searchParams.has(PARAM_NEW_WINDOW);
+  }, [pageUrl]);
 
   const showWakeLockEnabled = useCallback(() => {
     document.documentElement.classList.remove(styles.isInactive);
@@ -47,6 +69,12 @@ const HomePage: NextPage = () => {
       setSentinel(sentinel);
     }
   }, [isEnabled, sentinel]);
+
+  const handleClickOpenNewWindow = useCallback(() => {
+    const newUrl = new URL(pageUrl);
+    newUrl.searchParams.set(PARAM_NEW_WINDOW, '');
+    window.open(newUrl, undefined, FEATURES);
+  }, [pageUrl]);
 
   useEffect(() => {
     if (isActive) {
@@ -103,31 +131,53 @@ const HomePage: NextPage = () => {
         <link rel="icon" type="image/svg+xml" href={isActive ? svgActive : svgInactive} />
         <meta name="theme-color" content={isActive ? '#bef5a3' : '#f5a3a3'} />
       </Head>
-      <button
-        type="button"
-        className={styles.toggleButton}
-        onClick={handleClickToggle}
-        title={isActive ? 'Wake lock is enabled, click to disable' : 'Wake lock is disabled, click to enable'}
-        disabled={!isInitialized}
-      >
-        {
-          isActive ?
-            <Image
-              className={styles.indicatorIcon}
-              src={svgActive}
-              alt="Open eye icon"
-              width={64}
-              height={64}
-            /> :
-            <Image
-              className={styles.indicatorIcon}
-              src={svgInactive}
-              alt="Closed eye icon"
-              width={64}
-              height={64}
-            />
-        }
-      </button>
+      <div className={classNames(styles.container, isNewWindow && styles.containerNewWindow)}>
+        <div className={styles.main}>
+          <button
+            type="button"
+            className={styles.iconButton}
+            onClick={handleClickToggle}
+            title={isActive ? 'Wake lock is enabled, click to disable' : 'Wake lock is disabled, click to enable'}
+            disabled={!isInitialized}
+          >
+            {
+              isActive ?
+                <Image
+                  className={styles.icon}
+                  src={svgActive}
+                  alt="Open eye icon"
+                  width={64}
+                  height={64}
+                /> :
+                <Image
+                  className={styles.icon}
+                  src={svgInactive}
+                  alt="Closed eye icon"
+                  width={64}
+                  height={64}
+                />
+            }
+          </button>
+        </div>
+        {isNewWindow ? null : (
+          <div className={styles.actions}>
+            <button
+              type="button"
+              className={styles.iconButton}
+              onClick={handleClickOpenNewWindow}
+              title="Open in new window"
+            >
+              <Image
+                className={styles.icon}
+                src={svgNewWindow}
+                alt="New window icon"
+                width={64}
+                height={64}
+              />
+            </button>
+          </div>
+        )}
+      </div>
     </>
   );
 };
