20808156cf44c54751de177d44567ee6f05f6e2b
j- <j@skeoh.com>
2025-11-22 17:47:59 +1100

    Show menu items conditionally

--- src/ActionMenu.tsx
+++ src/ActionMenu.tsx
@@ -3,9 +3,14 @@ import { type RefObject } from 'react';
 import { ActionMenuItemAutoDisableTimer } from './actions/action-auto-disable-timer';
 import { ActionMenuItemBattery } from './actions/action-battery';
 import { ActionMenuItemBlackScreen } from './actions/action-black-screen';
+import { ActionMenuItemExpandCollapse } from './actions/action-expand-collapse';
 import { ActionMenuItemFullscreen } from './actions/action-fullscreen';
 import { ActionMenuItemNewWindow } from './actions/action-new-window';
+import { ActionMenuItemScroll } from './actions/action-scroll';
 import { ActionMenuItemShowPiP } from './actions/action-show-pip';
+import { useBattery } from './context/BatteryManagerContext';
+import { usePictureInPictureOpener } from './context/PictureInPictureOpenerContext';
+import { useAppContext } from './controller';
 
 export type ActionMenuProps<T extends HTMLElement> = {
   open: boolean;
@@ -18,6 +23,62 @@ export const ActionMenu = <T extends HTMLElement>({
   anchorRef,
   onClose,
 }: ActionMenuProps<T>) => {
+  const {
+    canExpandCollapse,
+    canFullscreen,
+    canNewWindow,
+    canPictureInPicture,
+    canScroll,
+    canStartTimer,
+  } = useAppContext();
+
+  const { isPictureInPictureWindowOpen } = usePictureInPictureOpener();
+
+  const battery = useBattery();
+
+  const showButtonScroll = canScroll;
+
+  const itemScroll = showButtonScroll ? (
+    <ActionMenuItemScroll onClick={onClose} />
+  ) : null;
+
+  const showButtonShowPiP =
+    canPictureInPicture && !isPictureInPictureWindowOpen;
+
+  const itemShowPiP = showButtonShowPiP ? (
+    <ActionMenuItemShowPiP onClick={onClose} />
+  ) : null;
+
+  const showButtonNewWindow = canNewWindow;
+
+  const itemNewWindow = showButtonNewWindow ? (
+    <ActionMenuItemNewWindow onClick={onClose} />
+  ) : null;
+
+  const showAutoDisableTimer = canStartTimer;
+
+  const itemAutoDisableTimer = showAutoDisableTimer ? (
+    <ActionMenuItemAutoDisableTimer onClick={onClose} />
+  ) : null;
+
+  const showButtonExpandCollapse = canExpandCollapse;
+
+  const itemExpandCollapse = showButtonExpandCollapse ? (
+    <ActionMenuItemExpandCollapse onClick={onClose} />
+  ) : null;
+
+  const showButtonFullscreen = canFullscreen;
+
+  const itemFullscreen = showButtonFullscreen ? (
+    <ActionMenuItemFullscreen onClick={onClose} />
+  ) : null;
+
+  const itemBlackScreen = <ActionMenuItemBlackScreen onClick={onClose} />;
+
+  const itemBattery = battery && (battery.level < 1 || !battery.charging) ? (
+    <ActionMenuItemBattery onClick={onClose} />
+  ) : null;
+
   return (
     <Menu
       anchorEl={() => anchorRef.current}
@@ -37,12 +98,14 @@ export const ActionMenu = <T extends HTMLElement>({
         },
       }}
     >
-      <ActionMenuItemAutoDisableTimer onClick={onClose} />
-      <ActionMenuItemBattery onClick={onClose} />
-      <ActionMenuItemBlackScreen onClick={onClose} />
-      <ActionMenuItemFullscreen onClick={onClose} />
-      <ActionMenuItemNewWindow onClick={onClose} />
-      <ActionMenuItemShowPiP onClick={onClose} />
+      {itemFullscreen}
+      {itemExpandCollapse}
+      {itemAutoDisableTimer}
+      {itemNewWindow}
+      {itemShowPiP}
+      {itemBlackScreen}
+      {itemBattery}
+      {itemScroll}
     </Menu>
   );
 };
