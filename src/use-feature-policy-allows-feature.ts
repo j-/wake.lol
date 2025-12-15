9eff01a42c74055d6835a9ea73674583d665ddcd
j- <j@skeoh.com>
2025-12-15 18:28:40 +1100

    Add "use feature policy allows feature" hook

---
+++ src/use-feature-policy-allows-feature.ts
@@ -0,0 +1,39 @@
+import { useDocument } from './context/WindowContext';
+
+interface FeaturePolicy {
+  allowsFeature?: (feature: string) => boolean;
+}
+
+declare global {
+  interface Document {
+    featurePolicy?: FeaturePolicy;
+    permissionsPolicy?: FeaturePolicy;
+    policy?: FeaturePolicy;
+  }
+}
+
+/**
+ * Determines if a particular feature is allowed by the document's feature
+ * policy.
+ *
+ * @param feature Feature name e.g. `fullscreen`.
+ * @returns
+ * - `true` if allowed
+ * - `false` if disallowed
+ * - `null` if feature policy is not supported or detection failed
+ */
+export const useFeaturePolicyAllowsFeature = (feature: string) => {
+  const document = useDocument();
+
+  const featurePolicy = (
+    document.permissionsPolicy ||
+    document.featurePolicy ||
+    document.policy
+  );
+
+  if (!featurePolicy || typeof featurePolicy.allowsFeature !== 'function') {
+    return null;
+  }
+
+  return featurePolicy.allowsFeature(feature);
+};
