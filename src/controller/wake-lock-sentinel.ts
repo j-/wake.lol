5514997f09037f7099c37384f87939b76c29c7cc
j- <j@skeoh.com>
2025-12-16 12:44:39 +1100

    Add catch block to fall back to video wake lock sentinel

--- src/controller/wake-lock-sentinel.ts
+++ src/controller/wake-lock-sentinel.ts
@@ -1,19 +1,15 @@
 import { getWakeLockScreenSentinel } from './wake-lock-screen-sentinel';
 import { getWakeLockVideoSentinel } from './wake-lock-video-sentinel';
 
-export const SENTINEL_TYPE = Symbol('sentinel_type');
-export const SENTINEL_TYPE_SCREEN = 'screen';
-export const SENTINEL_TYPE_VIDEO = 'video';
-
-export type WakeLockSentinelWithType = WakeLockSentinel & {
-  [SENTINEL_TYPE]: string;
-};
-
 export const getWakeLockSentinel = async (
   navigator: Navigator,
   document: Document,
-) => (
-  'wakeLock' in navigator ?
-    getWakeLockScreenSentinel(navigator) :
-    getWakeLockVideoSentinel(document)
-);
+) => {
+  try {
+    return 'wakeLock' in navigator ?
+      await getWakeLockScreenSentinel(navigator) :
+      await getWakeLockVideoSentinel(document);
+  } catch {
+    return getWakeLockVideoSentinel(document);
+  }
+};
