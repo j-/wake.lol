30a1a5e2f2e337636f4923b8f733c55cc73fc2db
j- <j@skeoh.com>
2025-12-29 00:59:15 +1100

    Add 192 and 512 icons and remove SVG from manifest

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,14 @@ const manifest: Partial<ManifestOptions> = {
   short_name: 'wake.lol',
   icons: [
     {
-      src: '/favicon.svg',
-      sizes: 'any',
-      type: 'image/svg+xml',
+      src: '/icon-192.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+    {
+      src: '/icon-512.png',
+      sizes: '512x512',
+      type: 'image/png',
     },
     {
       src: '/maskable-512.png',
