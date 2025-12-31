b0c1a43c10daad14c00fc4642a45592ca1e0c93c
j- <j@skeoh.com>
2025-12-31 18:47:20 +1100

    Remove bespoke "color swatch" icon

--- src/icons.tsx
+++ src/icons.tsx
@@ -21,6 +21,7 @@ import {
   Palette as IconPalette,
   PictureInPicture as IconPictureInPicture,
   Pipette as IconPipette,
+  Square as IconColorSwatch,
   SquarePlus as IconSquarePlus,
   Timer as IconTimer,
   Undo as IconUndo,
@@ -39,6 +40,7 @@ export {
   IconBattery4,
   IconBatteryCharging,
   IconClockFading,
+  IconColorSwatch,
   IconEllipsis,
   IconEye,
   IconEyeClosed,
@@ -116,20 +118,3 @@ export const IconBattery: FC<IconBatteryProps> = ({
   if (level < 0.8) return <IconBattery3 {...props} />;
   return <IconBattery4 {...props} />;
 };
-
-export const IconColorSwatch: FC<LucideProps> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="lucide lucide-gallery-thumbnails-icon lucide-gallery-thumbnails"
-    {...props}
-  >
-    <rect width="18" height="18" x="3" y="3" rx="2"/>
-  </svg>
-);
