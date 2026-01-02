b4afe4292ae0f88ec29558d7762c311201d53a27
j- <j@skeoh.com>
2026-01-02 19:45:14 +1100

    Change design of user activation dialog to sticky footer

--- src/UserActivationDialog.tsx
+++ src/UserActivationDialog.tsx
@@ -1,36 +1,87 @@
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { useId, type FC } from 'react';
+import Fade from '@mui/material/Fade';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import TrapFocus from '@mui/material/Unstable_TrapFocus';
+import { type FC } from 'react';
 import { useAppContext } from './controller';
+import { useSchemeColors } from './use-scheme-colors';
 
 export const UserActivationDialog: FC = () => {
   const { invokeUserActivation, requiresUserActivation } = useAppContext();
-  const id = useId();
+  const { bgColorEnabled } = useSchemeColors();
 
   return (
-    <Dialog
-      open={requiresUserActivation}
-      onClose={invokeUserActivation}
-      aria-labelledby={`${id}-title`}
-      aria-describedby={`${id}-description`}
-    >
-      <DialogTitle id={`${id}-title`}>
-        User activation required
-      </DialogTitle>
-      <DialogContent>
-        <DialogContentText id={`${id}-description`}>
-          Unable to automatically lock the screen without user activation. Please interact with the page (e.g., click a button) to proceed.
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={invokeUserActivation} autoFocus>
-          Proceed
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <>
+      <Backdrop
+        open={requiresUserActivation}
+        sx={{ zIndex: (theme) => theme.zIndex.modal - 1 }}
+        onClick={invokeUserActivation}
+      />
+
+      <Fade appear={false} in={requiresUserActivation}>
+        <Paper
+          role="dialog"
+          aria-modal="false"
+          aria-label="User activation warning"
+          square
+          variant="outlined"
+          tabIndex={-1}
+          sx={{
+            position: 'fixed',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            m: 0,
+            p: 2,
+            borderWidth: 0,
+            borderTopWidth: 1,
+            zIndex: (theme) => theme.zIndex.modal,
+          }}
+        >
+          <Stack
+            direction={{ xs: 'column', sm: 'row' }}
+            justifyContent='space-between'
+            gap={2}
+          >
+            <Box
+              flexShrink={1}
+              alignSelf={{ xs: 'flex-start', sm: 'center' }}
+            >
+              <Typography fontWeight="bold">
+                User activation required
+              </Typography>
+
+              <Typography variant="body2">
+                Unable to automatically lock the screen without user activation.
+                Please click or tap the page to proceed.
+              </Typography>
+            </Box>
+
+            <Stack
+              direction={{ xs: 'row-reverse', sm: 'row' }}
+              gap={2}
+              flexShrink={0}
+              alignSelf={{ xs: 'flex-end', sm: 'center' }}
+            >
+              <TrapFocus open={requiresUserActivation}>
+                <Button
+                  size="small"
+                  onClick={invokeUserActivation}
+                  variant="contained"
+                  sx={{ bgcolor: bgColorEnabled }}
+                  autoFocus
+                >
+                  Proceed
+                </Button>
+              </TrapFocus>
+            </Stack>
+          </Stack>
+        </Paper>
+      </Fade>
+    </>
   );
 };
