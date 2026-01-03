b6cb1b3c95f431648db0bc3d4927b8df3118c011
j- <j@skeoh.com>
2026-01-03 13:37:47 +1100

    Abstract app header into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
 import { type FC } from 'react';
+import { AppHeader } from './AppHeader';
 import { AppLayout } from './AppLayout';
 import { AppMetadata } from './AppMetadata';
-import { APP_NAME, BASE_URL, SKEOH_URL } from './constants';
 import Content from './content.mdx';
 import { useAppContext } from './controller';
 import { mdxComponents } from './mdx-components';
@@ -42,39 +40,14 @@ const App: FC = () => {
     );
   }
 
-  const header = (
-    <Box display="flex" justifyContent="space-between" alignItems="center">
-      <Typography component="h1" variant="h4">
-        <Link href={BASE_URL} color="inherit" sx={{ textDecoration: 'none' }}>
-          {APP_NAME}
-        </Link>
-      </Typography>
-
-      <Link
-        href={SKEOH_URL}
-        color="inherit"
-        underline="none"
-        target="_blank"
-        sx={{
-          opacity: 0.5,
-          transition: 'opacity 200ms',
-          '&:hover, &:focus': {
-            opacity: 1,
-          },
-        }}
-      >
-        skeoh.com
-      </Link>
-    </Box>
-  );
-
   return (
     <AppLayout
-      slotHeader={header}
+      slotHeader={<AppHeader />}
       slotActions={actionsContainer}
     >
       <AppMetadata />
       {userActivationDialog}
+
       <Stack maxWidth="70ch" my={4} mx="auto" gap={4}>
         <Box>
           <Content components={mdxComponents} />
---
+++ src/AppHeader.tsx
@@ -0,0 +1,36 @@
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import { type FC } from 'react';
+import { APP_NAME, BASE_URL, SKEOH_URL } from './constants';
+
+export const AppHeader: FC = () => (
+  <Box
+    display="flex"
+    justifyContent="space-between"
+    alignItems="center"
+    data-testid="AppHeader"
+  >
+    <Typography component="h1" variant="h4">
+      <Link href={BASE_URL} color="inherit" sx={{ textDecoration: 'none' }}>
+        {APP_NAME}
+      </Link>
+    </Typography>
+
+    <Link
+      href={SKEOH_URL}
+      color="inherit"
+      underline="none"
+      target="_blank"
+      sx={{
+        opacity: 0.5,
+        transition: 'opacity 200ms',
+        '&:hover, &:focus': {
+          opacity: 1,
+        },
+      }}
+    >
+      skeoh.com
+    </Link>
+  </Box>
+);
